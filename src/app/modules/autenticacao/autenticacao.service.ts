639035f0ad54eb49add839edeb9139b6a2836788
Matheus Ribeiro <omatheusribeiro@outlook.com>
2022-07-14 19:13:26 -0300

    Finalizado service de autenticação

---
+++ src/app/modules/autenticacao/autenticacao.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  autenticar(usuario: string, senha: string): Observable<any>{
+    return this.httpClient.post(environment.url.toString() + 'user/login', {
+      userName: usuario,
+      password: senha
+    })
+  }
+}
